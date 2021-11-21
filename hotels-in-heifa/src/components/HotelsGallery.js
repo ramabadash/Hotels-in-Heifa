a58a4eb24355619271873ca3e28609670bd9a89b
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-21 14:11:17 +0200

    # Defining HotelGallery as a route +  Defining HotelCard as a link

--- hotels-in-heifa/src/components/HotelsGallery.js
+++ hotels-in-heifa/src/components/HotelsGallery.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { hotelData } from '../data/hotels';
 import { HotelCard } from './HotelCard';
+import { Link } from 'react-router-dom';
+import { kebabCase } from '../helpers/helpers';
 
 export class HotelsGallery extends Component {
   render() {
     return (
       <div>
+        <h1> Hotels Gallery</h1>
         <ul className='hotels-list'>
           {hotelData.map((hotel) => (
-            <HotelCard key={hotel.name} name={hotel.name} img={''} />
+            <Link key={hotel.name} to={'/' + kebabCase(hotel.name)}>
+              {<HotelCard key={hotel.name} name={hotel.name} img={''} />}
+            </Link>
           ))}
         </ul>
       </div>
--- hotels-in-heifa/src/index.js
+++ hotels-in-heifa/src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import HotelsGallery from './components/HotelsGallery';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HotelsGallery />
+    <Router>
+      <Routes>
+        <Route exect path='/' element={<HotelsGallery />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
